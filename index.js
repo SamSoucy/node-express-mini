fe7268c150addb68cc159bb389da04b667eac28f
Samsoucy <newf930@outlook.com>
2019-02-18 16:39:53 -0500

    added get for returning an array of user objects

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // implement your API here
+const express = require("express");
+
+const db = require("./data/db.js");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/api/users", (req, res) => {
+    db.find()
+        .then(user => {
+            res.status(200).json(user);
+        })
+        .catch(({ code, message }) => {
+            res.status(code).json({
+                success: false,
+                message,
+            });
+        });
+})
+
+
+
+server.listen(3000, () => {
+    console.log("\n***Server Running on http://localhost:3000 ***\n")
+});
