d3882c4d4eb6490b2fd5549f35d6ace69a70c6c4
Samsoucy <newf930@outlook.com>
2019-02-18 17:50:12 -0500

    added a get for user id

--- index.js
+++ index.js
@@ -10,15 +10,31 @@ server.use(express.json());
 server.get("/api/users", (req, res) => {
     db.find()
         .then(user => {
-            res.status(200).json(user);
+            res.status(200).json({ success: true, user });
         })
-        .catch(({ code, message }) => {
-            res.status(code).json({
-                success: false,
-                message,
-            });
+        .catch(err => {
+            res.status(500).json({ success: false, error: "The user information could not be retrieved." })
         });
 })
+
+server.get("/api/users/:id", (req, res) => {
+    const {id} = req.params;
+    db.findById(id)
+        .then(user => {
+            if (user) {
+                res.status.apply(200).json({ success: true, user });
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: "The user with the specified ID does not exist."
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ success: false, error: "The user information could not be retrieved." })
+        });
+});
+
 
 
 
