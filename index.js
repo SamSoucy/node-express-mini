42c37a3b5e09f1604f5cdd91cc5886c204a28b12
Samsoucy <newf930@outlook.com>
2019-02-18 19:04:07 -0500

    added put for update

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const server = express();
 
 server.use(express.json());
 
+
+
 server.get("/api/users", (req, res) => {
     db.find()
         .then(user => {
@@ -73,9 +75,27 @@ server.delete("/api/users/:id", (req, res) => {
         });
 })
 
-
-
-
+server.put("/api/users/:id", (req, res) => {
+    const user = req.body;
+    const { id } = req.params;
+    if (user.name && user.bio) {
+        db.update(id, user)
+            .then(count => {
+                if (count) {
+                    db.findById(id).then(user => {
+                        res.json(user);
+                    });
+                } else {
+                    res.status(404).json({ message: "The user with the specified ID does not exist." });
+                }
+            })
+            .catch(err => {
+                res.status(500).json({ error: "The user information could not be modified." });
+            });
+    } else {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
+    }
+});
 
 
 
